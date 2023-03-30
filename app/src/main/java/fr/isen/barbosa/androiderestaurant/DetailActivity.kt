64ec3e88387f3a5e8cc0a61020978f54b205e157
struixll <ileona-barbosa@hotmail.fr>
2023-03-30 22:03:43 +0200

    Ajout des ingrédients

--- app/src/main/java/fr/isen/barbosa/androiderestaurant/DetailActivity.kt
+++ app/src/main/java/fr/isen/barbosa/androiderestaurant/DetailActivity.kt
@@ -2,6 +2,7 @@ package fr.isen.barbosa.androiderestaurant
 
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import fr.isen.barbosa.androiderestaurant.databinding.ActivityDetailBinding
@@ -25,17 +26,40 @@ class DetailActivity : AppCompatActivity() {
         //Récupération du nom du plat
         val title = item.nameFr
         supportActionBar?.title=title
-        //val ingredients = item.ingredients ?:""
+        val ingredients = item.ingredients
+
+        var compteur = 0
 
         //appel du Carrousel
         val viewPager = findViewById<ViewPager>(R.id.imageDetail)
         val leCarrouselAdapter = CarrouselAdapter(this, this.item.images)
         viewPager.adapter = leCarrouselAdapter
 
+        if (item.ingredients.isNotEmpty()){
+            val ingredientsString = StringBuilder()
+            ingredients.forEach { ingredients ->
+                ingredientsString.append(ingredients.nameFr)
+                ingredientsString.append("\n")
+            }
+            binding.ingredients.text = ingredientsString
 
+            binding.plusButton.setOnClickListener {
+                compteur++
+                binding.quantity.text = Editable.Factory.getInstance().newEditable(compteur.toString())
+                val addition = compteur * item.prices[0].price?.toDouble()!!
+                binding.addition.text = addition.toString()
+            }
 
+            binding.lessButton.setOnClickListener {
+                compteur--
+                binding.quantity.text = Editable.Factory.getInstance().newEditable(compteur.toString())
+                val addition = compteur * item.prices[0].price?.toDouble()!!
+                binding.addition.text = addition.toString()
+            }
+
+            binding.quantity.text = compteur.toString()
+        }
     }
-
 
 
     override fun onSupportNavigateUp(): Boolean {
